7189ed77fcfdd6889719877b8b0753ee1cc229d9
hphilamore <hemmaphilamore@gmail.com>
2023-04-20 13:11:04 +0100

    motor shield demo code tidied up

--- motor_shield_test.py
+++ motor_shield_test.py
@@ -2,87 +2,101 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
-# Set the GPIO modes
+
+# Set the GPIO mode (pin numbers)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Set variables for the GPIO motor pins
-pinMotorAForwards = 6#10
-pinMotorABackwards = 13#9
-pinMotorBForwards = 20#8
-pinMotorBBackwards = 21#7
-EnableA = 12 #21
-EnableB = 26 #20
+Motor1A = 6
+Motor1B = 13
+Motor2A = 20
+Motor2B = 21
+EnableA = 12
+EnableB = 26
 #pinTrigger = 17
 #pinEcho = 18
 
 # How many times to turn the pin on and off each second
 Frequency = 20
-# How long the pin stays on each cycle, as a percent (here, it's 30%)
-DutyCycle = 90
+
+# How long the pin stays on each cycle, as a percent 
+DutyCycle = 50
+
 # Setting the duty cycle to 0 means the motors will not turn
 Stop = 0
 
 # Set the GPIO Pin mode to be Output
-GPIO.setup(pinMotorAForwards, GPIO.OUT)
-GPIO.setup(pinMotorABackwards, GPIO.OUT)
-GPIO.setup(pinMotorBForwards, GPIO.OUT)
-GPIO.setup(pinMotorBBackwards, GPIO.OUT)
+GPIO.setup(Motor1A, GPIO.OUT)
+GPIO.setup(Motor1B, GPIO.OUT)
+GPIO.setup(Motor2A, GPIO.OUT)
+GPIO.setup(Motor2B, GPIO.OUT)
 GPIO.setup(EnableA, GPIO.OUT)
 GPIO.setup(EnableB, GPIO.OUT)
 
 # Set the GPIO to software PWM at 'Frequency' Hertz
-pwmMotorAForwards = GPIO.PWM(pinMotorAForwards, Frequency)
-pwmMotorABackwards = GPIO.PWM(pinMotorABackwards, Frequency)
-pwmMotorBForwards = GPIO.PWM(pinMotorBForwards, Frequency)
-pwmMotorBBackwards = GPIO.PWM(pinMotorBBackwards, Frequency)
+PWM_Motor1A = GPIO.PWM(Motor1A, Frequency)
+PWM_Motor1B = GPIO.PWM(Motor1B, Frequency)
+PWM_Motor2A = GPIO.PWM(Motor2A, Frequency)
+PWM_Motor2B = GPIO.PWM(Motor2B, Frequency)
 
 # Start the software PWM with a duty cycle of 0 (i.e. not moving)
-pwmMotorAForwards.start(Stop)
-pwmMotorABackwards.start(Stop)
-pwmMotorBForwards.start(Stop)
-pwmMotorBBackwards.start(Stop)
+PWM_Motor1A.start(Stop)
+PWM_Motor1B.start(Stop)
+PWM_Motor2A.start(Stop)
+PWM_Motor2B.start(Stop)
 
 def Enable(state):
- GPIO.output(EnableA, state)
- GPIO.output(EnableB, state)
+    """ 
+    When state is 0, enable pin is driven high, PWM output
+    """
+    GPIO.output(EnableA, state)
+    GPIO.output(EnableB, state)
 
-# Turn all motors off
 def StopMotors():
-    pwmMotorAForwards.ChangeDutyCycle(Stop)
-    pwmMotorABackwards.ChangeDutyCycle(Stop)
-    pwmMotorBForwards.ChangeDutyCycle(Stop)
-    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+    """
+    Turn all motors off
+    """
+    PWM_Motor1A.ChangeDutyCycle(Stop)
+    PWM_Motor1B.ChangeDutyCycle(Stop)
+    PWM_Motor2A.ChangeDutyCycle(Stop)
+    PWM_Motor2B.ChangeDutyCycle(Stop)
 
-# Turn both motors forwards
 def Forwards():
-    pwmMotorAForwards.ChangeDutyCycle(DutyCycle)
-    pwmMotorABackwards.ChangeDutyCycle(Stop)
-    pwmMotorBForwards.ChangeDutyCycle(DutyCycle)
-    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+    """
+    Turn both motors forwards
+    """
+    PWM_Motor1A.ChangeDutyCycle(DutyCycle)
+    PWM_Motor1B.ChangeDutyCycle(Stop)
+    PWM_Motor2A.ChangeDutyCycle(DutyCycle)
+    PWM_Motor2B.ChangeDutyCycle(Stop)
 
-# Turn both motors backwards
 def Backwards():
-    pwmMotorAForwards.ChangeDutyCycle(Stop)
-    pwmMotorABackwards.ChangeDutyCycle(DutyCycle)
-    pwmMotorBForwards.ChangeDutyCycle(Stop)
-    pwmMotorBBackwards.ChangeDutyCycle(DutyCycle)
+    """
+    Turn both motors backwards
+    """
+    PWM_Motor1A.ChangeDutyCycle(Stop)
+    PWM_Motor1B.ChangeDutyCycle(DutyCycle)
+    PWM_Motor2A.ChangeDutyCycle(Stop)
+    PWM_Motor2B.ChangeDutyCycle(DutyCycle)
 
-# Turn left
 def Left():
-    pwmMotorAForwards.ChangeDutyCycle(Stop)
-    pwmMotorABackwards.ChangeDutyCycle(DutyCycle)
-    pwmMotorBForwards.ChangeDutyCycle(DutyCycle)
-    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+    """
+    Turn left
+    """
+    PWM_Motor1A.ChangeDutyCycle(Stop)
+    PWM_Motor1B.ChangeDutyCycle(DutyCycle)
+    PWM_Motor2A.ChangeDutyCycle(DutyCycle)
+    PWM_Motor2B.ChangeDutyCycle(Stop)
 
-# Turn Right
 def Right():
-    pwmMotorAForwards.ChangeDutyCycle(DutyCycle)
-    pwmMotorABackwards.ChangeDutyCycle(Stop)
-    pwmMotorBForwards.ChangeDutyCycle(Stop)
-    pwmMotorBBackwards.ChangeDutyCycle(DutyCycle)
-
-
+    """
+    Turn Right
+    """
+    PWM_Motor1A.ChangeDutyCycle(DutyCycle)
+    PWM_Motor1B.ChangeDutyCycle(Stop)
+    PWM_Motor2A.ChangeDutyCycle(Stop)
+    PWM_Motor2B.ChangeDutyCycle(DutyCycle)
 
 
 
